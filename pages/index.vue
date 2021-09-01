ebcfcfee7e05b5e934c8cf3e47a7bcde3ab4cfa3
h82st8 <h82st8@gmail.com>
2021-09-01 22:11:13 +0300

    fix: homepage

--- pages/index.vue
+++ pages/index.vue
@@ -9,12 +9,11 @@
 </template>
 
 <script>
-import dayjs from "dayjs";
-import { groupBy, prop, reverse } from "rambda";
-import getArrayFromObjectArray from "@/utils/getArrayFromObjectArray";
+import rowsGroupByDay from "@/mixins/rowsGroupByDay";
 
 export default {
   name: "HomePage",
+  mixins: [rowsGroupByDay],
   layout: "base",
   data() {
     return {
@@ -34,69 +33,84 @@ export default {
           timestamp: "1629474423",
           hash: "0x40160a8130ff838e5659d48b58b6c17651011f05",
           type: "Transfer",
-          from: "0x56440a8145330ff838e56",
-          to: "0x34673454358130ff838e71",
-          sum: 645334,
+          from: ["0x56440a8145330ff838e56"],
+          to: ["0x34673454358130ff838e71"],
+          sum: [
+            {
+              name: "EBP",
+              amount: 12,
+            },
+          ],
         },
         {
           id: "2",
           timestamp: "1629468603",
           hash: "0x5647830ff838e5659d48b58b636617651011f00",
           type: "Transfer",
-          from: "0x440a814533056ff838e56",
-          to: "0x67343454358130ff838e71",
-          sum: 64334,
+          from: ["0x440a814533056ff838e56"],
+          to: ["0x67343454358130ff838e71"],
+          sum: [
+            {
+              name: "EBP",
+              amount: 241,
+            },
+            {
+              name: "RANK",
+              amount: 2,
+            },
+          ],
         },
         {
           id: "3",
           timestamp: "1629379680",
           hash: "0x40160a8130ff838e5659d48b58b6c17651011f05",
           type: "Distribution",
-          from: "0x440a814533056ff838e56",
-          to: 235,
-          sum: 445,
+          from: ["0x440a814533056ff838e56", "0x67343454358130ff838e71"],
+          to: [
+            "0x67343454358130ff838e71",
+            "0x67343454358130ff838e71",
+            "0x67343454358130ff838e71",
+            "0x67343454358130ff838e71",
+            "0x67343454358130ff838e71",
+          ],
+          sum: [
+            {
+              name: "EBP",
+              amount: 241,
+            },
+          ],
         },
         {
           id: "4",
           timestamp: "1629376868",
           hash: "0x5647830ff838e5659d48b58b636617651011f00",
           type: "Distribution",
-          from: "0x440a814533056ff838e56",
-          to: 232,
-          sum: 45,
+          from: [
+            "0x440a814533056ff838e56",
+            "0x67343454358130ff838e71",
+            "0x67343454358130ff838e71",
+            "0x67343454358130ff838e71",
+            "0x67343454358130ff838e71",
+            "0x67343454358130ff838e71",
+          ],
+          to: ["0x67343454358130ff838e71", "0x67343454358130ff838e71"],
+          sum: [
+            {
+              name: "EBP",
+              amount: 241,
+            },
+            {
+              name: "RANK",
+              amount: 2,
+            },
+            {
+              name: "CP",
+              amount: 2,
+            },
+          ],
         },
       ],
-      format: {
-        day: "YYYY-MM-DD",
-        time: "HH:mm",
-      },
     };
-  },
-  computed: {
-    rowsGroupByDay() {
-      const mappedRows = this.transactionRows.map(({ ...row }) => ({
-        ...row,
-
-        id: row.id,
-        date: row.timestamp * 1000,
-        hash: row.hash,
-        type: row.type,
-        from: row.from,
-        to: row.to,
-        sum: row.sum,
-        dateDay: String(
-          dayjs(
-            dayjs.unix(row.timestamp).format(this.format.day),
-            this.format.day
-          ).unix()
-        ),
-        time: dayjs.unix(row.timestamp).format(this.format.time),
-      }));
-      const rowsObjGroupByDay = groupBy(prop("dateDay"), mappedRows);
-      const rowsArrayGroupByDay = getArrayFromObjectArray(rowsObjGroupByDay);
-
-      return reverse(rowsArrayGroupByDay);
-    },
   },
 };
 </script>
